a90d9edfba926df1f985d2d233bd7bf167239a10
U0181792 <john.hunsley@thomsonreuters.com>
2019-02-18 17:38:20 +0000

    test for persistent inheritence model

--- src/test/java/com/hunsley/account/AccountRepositoryTest.java
+++ src/test/java/com/hunsley/account/AccountRepositoryTest.java
@@ -1,12 +1,42 @@
 package com.hunsley.account;
 
+import com.hunsley.account.model.Account;
+import com.hunsley.account.model.CurrentAccount;
+import com.hunsley.account.repository.AccountRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AccountRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
 
 
     @Test
     public void simpleWriteRead() {
+        CurrentAccount currentAccount = new CurrentAccount();
+        currentAccount.setUid(10);
+        currentAccount.setValue(103.34);
+        currentAccount.setOverdraftLimit(400.00);
+        accountRepository.save(currentAccount);
+        assertTrue(currentAccount.getAccountId() > 0);
+        Long id = currentAccount.getAccountId();
+
+        Optional<Account> value = accountRepository.findById(id);
+        assertTrue(value.isPresent());
+        Account read = value.get();
+        assertTrue(read instanceof CurrentAccount);
+        assertTrue(read.getAccountId() == id);
 
     }
 }
