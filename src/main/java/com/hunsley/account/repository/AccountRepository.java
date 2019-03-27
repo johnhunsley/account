32b792ddbe46018c34316c1da6c5edb90719064f
jhunsley <john.hunsley@capgemini.com>
2019-03-27 10:55:59 +0000

    CORS on Rest Resource and Filter to add CORS headers for all requests

--- src/main/java/com/hunsley/account/repository/AccountRepository.java
+++ src/main/java/com/hunsley/account/repository/AccountRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ import java.util.List;
  * </p>
  */
 @Profile("service")
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "accounts", path = "accounts")
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
 
